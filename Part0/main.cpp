811dfbdab92a2de9055a582ac5cd68352378f800
IanGaffney <35350577+IanGaffney@users.noreply.github.com>
2018-01-18 15:48:02 -0800

    Re-org and final release
    Moved everything up a directory.
    Finished the homework

--- Part0/main.cpp
+++ Part0/main.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <cassert>
 
 int main()
 {
@@ -10,12 +11,11 @@ int main()
 	std::size_t signedCharSize		{ sizeof(signed char) };
 	std::size_t unsignedCharSize	{ sizeof(unsigned char) };
 	std::size_t charSize			{ sizeof(char) };
-	std::size_t unsignedShortSize1	{ sizeof(unsigned short) };
 	std::size_t shortSize			{ sizeof(short) };
 	std::size_t signedSize			{ sizeof(signed) };
 	std::size_t unsignedSize		{ sizeof(unsigned) };
 	std::size_t intSize				{ sizeof(int) };
-	std::size_t unsignedShortSize2	{ sizeof(unsigned short) }; // unsigned short was asked for twice
+	std::size_t unsignedShortSize2	{ sizeof(unsigned short) };
 	std::size_t size_tSize			{ sizeof(std::size_t) };
 	std::size_t signedLongSize		{ sizeof(signed long) };
 	std::size_t unsignedLongSize	{ sizeof(unsigned long) };
@@ -24,10 +24,14 @@ int main()
 	std::size_t doubleSize			{ sizeof(double) };
 	std::size_t longDoubleSize		{ sizeof(long double) };
 
-
-
-
-
+	assert(sizeof(char) == 1);
+	assert(sizeof(float) == 4);
+	assert(sizeof(double) == 8);
+	assert(sizeof(bool) == 1);
+	assert(sizeof(short) != 1);
+	assert(sizeof(long) != 3);
+	assert(sizeof(long double) != 4);
+	
 	return 0;
 }
 
--- README.md
+++ README.md
@@ -29,7 +29,6 @@ Create a project named "Part0" that calculates the size of the following data ty
 * signed char
 * unsigned char
 * char
-* unsigned short
 * short
 * signed
 * unsigned
@@ -50,6 +49,14 @@ Batch build all x86, 64 bit and debug, release versions of your code.
 
 In the README.md file, note for each assertion if it passes or fails the assertion test regardless of the build configuration.  
 
+assert(sizeof(char) == 1)			Passes
+assert(sizeof(float) == 4)			Passes
+assert(sizeof(double) == 8)			Passes
+assert(sizeof(bool) == 1)			Passes
+assert(sizeof(short) != 1)			Passes
+assert(sizeof(long) != 2)			Passes
+assert(sizeof(long double) != 4)	Passes
+
 Part 1:  
 
 Create a project named "Part1" Calculate the size of the following literals and print the value and size to std::cout:  
@@ -63,7 +70,7 @@ Create a project named "Part1" Calculate the size of the following literals and 
 * ‘\0’
 * ‘\377’
 * 0.0
-* 0f
+* 0.f
 * "Annie" " " "\\"Lennox\\"\n"
 
 Terminate each output with a non-flushing newline.  
